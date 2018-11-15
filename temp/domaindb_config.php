edc1b0cde2dc6e18532b9d472e36e5e0e377f835
Nicholas <thortwo@outlook.com>
2018-11-15 15:14:30 +0800

    ignore
    ignore

--- temp/domaindb_config.php
+++ temp/domaindb_config.php
@@ -6,17 +6,5 @@ return array (
     'cid' => '64',
     'dirname' => 'news',
   ),
-  'localhost:8888' => 
-  array (
-    'id' => 5,
-    'cid' => '64',
-    'dirname' => 'news',
-  ),
-  'www.example2.com' => 
-  array (
-    'id' => 6,
-    'cid' => '52',
-    'dirname' => 'novel',
-  ),
 );
 ?>
